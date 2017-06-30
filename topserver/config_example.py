9d435ffaecf5877cfde55b69f31f5cf1550ee919
Justin Fu <justinjfu@gmail.com>
2017-06-29 18:25:56 -0700

    update

---
+++ topserver/config_example.py
@@ -0,0 +1,1 @@
+HOSTS = ['localhost', 'my.server.com', 'my.server2.com']
---
+++ topserver/run_server.py
@@ -0,0 +1,9 @@
+"""
+Backend server which returns monitoring data at the following endpoints:
+
+/top_data
+    (Returns formatted moving average data)
+/nvidia_data
+    (Returns formatted moving average data)
+
+"""
